535ab4d96d8d5298ae38e038dee620d148ae9cd3
RasoulMousaviDev <rasoulmousavidev@gmail.com>
2024-08-16 23:13:08 +0330

    develop OTP Code controller

--- api/app/Http/Controllers/OTPCodeController.php
+++ api/app/Http/Controllers/OTPCodeController.php
@@ -2,21 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GenerateOTPCodeRequest;
+use App\Http\Requests\VerifyOTPCodeRequest;
 use App\Models\OTPCode;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class OTPCodeController extends Controller
 {
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function generate(Request $request) {}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function verify(Request $request)
+    public function generate(GenerateOTPCodeRequest $request)
+    {
+
+        $type = $request->input('type'); // email or phone
+
+        $code = random_int(100000, 999999);
+
+        //user
+
+        OTPCode::firstOrCreate([
+            $type => $request->get($type)
+        ], [
+            'code' => $code,
+            'expires_at' => '',
+            'type' => $type
+        ]);
+    }
+
+
+    public function verify(VerifyOTPCodeRequest $request)
     {
         // $user = User::first();
         // $token = auth()->claims(['foo' => 'bar'])->login($user);
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OTPCodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->middleware('auth:api')->prefix('auth')->group(function () {
@@ -8,4 +9,9 @@ Route::controller(AuthController::class)->middleware('auth:api')->prefix('auth')
     Route::post('me', 'me');
     Route::post('refresh', 'refresh');
     Route::post('logout', 'logout');
+});
+
+Route::controller(OTPCodeController::class)->prefix('otp-code')->group(function () {
+    Route::get('', 'generate');
+    Route::post('', 'verify');
 });
