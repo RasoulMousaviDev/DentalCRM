7d2585d6e09fe33854ff2c57e5bae86e576fd618
rasoul-mousavi-dev <r.mousavi@touric.com>
2024-11-30 23:23:07 +0330

    debugging

--- spa/src/components/PatientInfo.vue
+++ spa/src/components/PatientInfo.vue
@@ -10,8 +10,8 @@
         </div>
         <ul v-if="data" class="grid grid-cols-5 border-t translate-y-4">
             <li v-for="(key, i) in keys" :key="key"
-                class="flex items-center justify-between gap-2 px-4 py-3 last:justify-start"
-                :class="{ 'border-b': i < 13, 'col-span-2': i == 10 || i == 11, 'col-span-full': i == 14, 'border-l': ![4, 9, 12, 13].includes(i) }">
+                class="flex items-center justify-between gap-2 px-4 py-3"
+                :class="{ 'border-b': i < 13, 'col-span-2': i == 10 || i == 11, 'col-span-4 !justify-start': i == 13, 'border-l': ![4, 9, 12, 14].includes(i) }">
                 <span class="opacity-70">{{ $t(key.replace('_', '-')) }}:</span>
                 <div v-if="key == 'mobiles'" class="flex gap-2">
                     <Chip v-for="mobile in data.mobiles" :label="mobile.number" />
@@ -20,6 +20,9 @@
                     {{ data.treatments.map(({ title }) => title).join(' | ') }}
                 </div>
                 <Tag v-else-if="key == 'status'" v-bind="data.status" />
+                <span v-else-if="key == 'insurance'">
+                    {{ $t(data.insurance ? 'has' : 'not-has') }}
+                </span>
                 <span v-else class="font-medium" :class="{ 'ltr': (i + 1) % 5 === 0 }">
                     {{
                         key === 'gender' ? $t(data[key]) : (data[key]?.title || data[key])
@@ -42,7 +45,7 @@ const store = usePatientsStore()
 
 const keys = reactive([
     'firstname', 'lastname', 'birthday', 'gender', 'created_at', 'province', 'city',
-    'telephone', 'lead_source', 'updated_at', 'mobiles', 'treatments', 'status', 'desc'
+    'telephone', 'lead_source', 'updated_at', 'mobiles', 'treatments', 'insurance', 'desc', 'status'
 ])
 
 const PatientForm = defineAsyncComponent(() => import('@/components/PatientForm.vue'));
@@ -55,7 +58,7 @@ const edit = async (data) => {
     patient.province = patient.province.id
     patient.city = patient.city.id
     patient.status = patient.status.id
-    patient.lead_source = patient.lead_source.id 
+    patient.lead_source = patient.lead_source.id
 
     dialog.open(PatientForm, {
         props: { header: t('editPatient'), modal: true },
